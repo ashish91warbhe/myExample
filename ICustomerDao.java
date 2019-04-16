40cac47cd736d3e6af842e3309bfbb899a29ffed
ashish91warbhe@gmail.com <ashish91warbhe@gmail.com>
2019-04-16 14:59:53 +0530

    fiirst commit

---
+++ ICustomerDao.java
@@ -0,0 +1,17 @@
+package com.app.dao;
+
+import java.util.List;
+
+import com.app.model.Customer;
+
+
+
+public interface ICustomerDao {
+	public int saveCustomer(Customer cust);
+	public void updateCustomer(Customer cust);
+	public void deleteCustomer(int custId);
+	public Customer getCustomerById(int custId);
+	public List<Customer> getAllCustomer();
+
+
+}
---
+++ IDocumentDao.java
@@ -0,0 +1,10 @@
+package com.app.dao;
+
+import java.util.List;
+
+import com.app.model.Document;
+
+public interface IDocumentDao {
+public int saveDocument(Document doc);
+public List<Object[]> getDocIdAndName();
+}
